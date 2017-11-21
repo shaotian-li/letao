c370dc8101f99c76362b98f50f9c9fa5484ae9aa
lz <lz168@.com>
2017-11-21 21:58:07 +0800

    index.html基本功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -5,16 +5,64 @@ $(function() {
 
     // 在ajax开启请求之前，让进度条显示
     $(window).ajaxStart(function(){
-        console.log(111);
+        // console.log(111);
         // 开启进度条
         NProgress.start();
     });
 
     // 在ajax关闭请求之后，让进度条关闭
     $(window).ajaxStart(function(){
-        console.log(222);
+        // console.log(222);
         // 关闭进度条
         NProgress.done();
     });
   
+    // 非登录页面 判读用户是否登录了 如果登录了就继续 如果没有登录就跳到 login.html
+
+    if(location.href.indexOf("login.html")== -1){
+        $.ajax({
+            type:"get",
+            url:"/employee/checkRootLogin",
+            success:function(data){
+                console.log(data);
+              if(data.error == 400){
+                  location.href ="login.html";
+              }
+            }
+        })
+    }
+
+
+    //侧边栏二级菜单显示隐藏
+    $(".child").prev().on("click",function(){
+        $(this).next().slideToggle();
+    });
+
+    // 侧边栏显示隐藏
+    $(".icon-menu").on("click",function(){
+        $(".lt_aside").toggleClass("now");
+        $(".lt_section").toggleClass("now");
+    });
+
+
+    // 退出功能
+    $(".icon_logout").on("click",function(){
+        $("#logoutModal").modal("show");
+    });
+
+    // 给退出按钮注册退出事件
+    $(".btn_logout").on("click",function(){
+        // 发送ajax 退出系统
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            success:function(data){
+                console.log(data);
+                if(data.success==true){
+                    location.href="login.html"
+                }
+            }
+        })
+    })
+
 });
